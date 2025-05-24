07c69932ee6473a1d70e4313746667ce5a50e486
Benno <77901422+BennoMLRP0908@users.noreply.github.com>
2025-05-24 13:20:02 +0100

    Removal of Ad Widget

--- resources/views/templates/wrapper.blade.php
+++ resources/views/templates/wrapper.blade.php
@@ -1,117 +1,55 @@
 ﻿<!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>{{ config('app.name', 'PhoenixPanel') }} - @yield('title')</title>
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <meta name="_token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'PhoenixPanel') }}</title>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
-        <link rel="manifest" href="/favicons/manifest.json">
-        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
-        <link rel="shortcut icon" href="/favicons/favicon.ico">
-        <meta name="msapplication-config" content="/favicons/browserconfig.xml">
-        <meta name="theme-color" content="#E25822">
+        @section('meta')
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+            <meta name="csrf-token" content="{{ csrf_token() }}">
+            <meta name="robots" content="noindex">
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+            <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+            <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+            <link rel="manifest" href="/favicons/manifest.json">
+            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
+            <link rel="shortcut icon" href="/favicons/favicon.ico">
+            <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+            <meta name="theme-color" content="#E25822"> <!-- Phoenix primary color -->
+        @show
+
+        @section('user-data')
+            @if(!is_null(Auth::user()))
+                <script>
+                    window.PhoenixPanelUser = {!! json_encode(Auth::user()->toVueObject()) !!};
+                </script>
+            @endif
+            @if(!empty($siteConfiguration))
+                <script>
+                    window.SiteConfiguration = {!! json_encode($siteConfiguration) !!};
+                </script>
+            @endif
+        @show
+        <style>
+            @import url('//fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap');
+            @import url('//fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:500&display=swap');
+        </style>
+
+        @yield('assets')
 
         @include('layouts.scripts')
-
-        @section('scripts')
-            {!! Theme::css('vendor/select2/select2.min.css?t={cache-version}') !!}
-            {!! Theme::css('vendor/bootstrap/bootstrap.min.css?t={cache-version}') !!}
-            {!! Theme::css('vendor/adminlte/admin.min.css?t={cache-version}') !!}
-            {!! Theme::css('vendor/adminlte/colors/skin-phoenix.min.css?t={cache-version}') !!}
-            {!! Theme::css('vendor/sweetalert/sweetalert.min.css?t={cache-version}') !!}
-            {!! Theme::css('vendor/animate/animate.min.css?t={cache-version}') !!}
-            {!! Theme::css('css/phoenixpanel.css?t={cache-version}') !!}
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-        @show
     </head>
-    <body class="hold-transition skin-blue sidebar-mini">
-        <div class="wrapper">
-            <header class="main-header">
-                <a href="{{ route('index') }}" class="logo">
-                    <span>{{ config('app.name', 'PhoenixPanel') }}</span>
-                </a>
-                <nav class="navbar navbar-static-top">
-                    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-                </nav>
-            </header>
-            <aside class="main-sidebar">
-                <section class="sidebar">
-                    <ul class="sidebar-menu">
-                        <li class="header">MAIN NAVIGATION</li>
-                        <li>
-                            <a href="{{ route('index') }}">
-                                <i class="fa fa-home"></i> <span>Home</span>
-                            </a>
-                        </li>
-                    </ul>
-                </section>
-            </aside>
-            <div class="content-wrapper">
-                <section class="content">
-                    @yield('content')
-                </section>
-            </div>
-            <footer class="main-footer">
-                <div class="pull-right small text-gray" style="margin-right:10px;margin-top:-7px;">
-                    <strong><i class="fa fa-fw fa-code-fork"></i></strong> {{ config('app.version', '0.0.0') }}<br />
-                    <strong><i class="fa fa-fw fa-clock-o"></i></strong> {{ round(microtime(true) - LARAVEL_START, 3) }}s
-                </div>
-                Copyright &copy; 2015 - {{ date('Y') }} <a href="https://phoenixpanel.io/">PhoenixPanel</a>.
-            </footer>
-        </div>
-
-        <!-- Left Ad Display -->
-        @if(config('phoenixpanel.ads.enabled') && !\Illuminate\Support\Str::startsWith(request()->path(), 'admin/'))
-            <div class="ad-display" style="position: fixed; left: 0; top: 50%; transform: translateY(-50%); z-index: 1000;">
-                <div style="width: 160px; height: 600px; border: 1px solid #ccc; margin: 10px 0;">
-                    {!! config('phoenixpanel.ads.code') !!}
-                </div>
-            </div>
-        @endif
-
-        <!-- Right Ad Display -->
-        @if(config('phoenixpanel.ads.enabled') && !\Illuminate\Support\Str::startsWith(request()->path(), 'admin/'))
-            <div class="ad-display" style="position: fixed; right: 0; top: 50%; transform: translateY(-50%); z-index: 1000;">
-                <div style="width: 160px; height: 600px; border: 1px solid #ccc; margin: 10px 0;">
-                    {!! config('phoenixpanel.ads.code') !!}
-                </div>
-            </div>
-        @endif
-
-        @section('footer-scripts')
-            <style>
-                @media (max-width: 768px) {
-                    .ad-display {
-                        width: 100px !important;
-                        height: 400px !important;
-                        margin: 5px 0 !important;
-                    }
-                    .ad-display div {
-                        width: 100% !important;
-                        height: 100% !important;
-                    }
-                }
-            </style>
-            {!! Theme::js('vendor/jquery/jquery.min.js?t={cache-version}') !!}
-            {!! Theme::js('vendor/sweetalert/sweetalert.min.js?t={cache-version}') !!}
-            {!! Theme::js('vendor/bootstrap/bootstrap.min.js?t={cache-version}') !!}
-            {!! Theme::js('vendor/slimscroll/jquery.slimscroll.min.js?t={cache-version}') !!}
-            {!! Theme::js('vendor/adminlte/app.min.js?t={cache-version}') !!}
-            {!! Theme::js('vendor/bootstrap-notify/bootstrap-notify.min.js?t={cache-version}') !!}
-            {!! Theme::js('vendor/select2/select2.full.min.js?t={cache-version}') !!}
-            {!! Theme::js('js/admin/functions.js?t={cache-version}') !!}
-            <script src="/js/autocomplete.js" type="application/javascript"></script>
+    <body class="{{ $css['body'] ?? 'bg-neutral-50' }}">
+        @section('content')
+            @yield('above-container')
+            @yield('container')
+            @yield('below-container')
+        @show
+        @section('scripts')
+            {!! $asset->js('main.js') !!}
         @show
     </body>
 </html>
+
+
