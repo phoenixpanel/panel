91df946ca95210f5e8a09e80e77361db1a7b8584
Benno <77901422+BennoMLRP0908@users.noreply.github.com>
2025-05-24 19:36:23 +0100

    Update global-ad.blade.php

--- resources/views/partials/global-ad.blade.php
+++ resources/views/partials/global-ad.blade.php
@@ -2,15 +2,22 @@
 <style>
 .ad-display {
     position: fixed;
-    left: 50%;
     top: 50%;
-    transform: translate(-50%, -50%);
+    transform: translateY(-50%);
     z-index: 1000;
     width: 160px;
     height: 600px;
     display: flex;
     align-items: center;
     justify-content: center;
+}
+
+.ad-display.left {
+    left: 25%;
+}
+
+.ad-display.right {
+    right: 25%;
 }
 
 @media (max-width: 768px) {
@@ -23,8 +30,13 @@
 }
 </style>
 
-    <div class="ad-display">
-        <div style="width: 100%; height: 100%; border: 1px solid #ccc;">
+    <div class="ad-display left">
+        <div style="width: 100%; height: 100%; border: 1px solid #ccc; margin: 10px 0;">
+            {!! config('phoenixpanel.ads.code') !!}
+        </div>
+    </div>
+    <div class="ad-display right">
+        <div style="width: 100%; height: 100%; border: 1px solid #ccc; margin: 10px 0;">
             {!! config('phoenixpanel.ads.code') !!}
         </div>
     </div>
